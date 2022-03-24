dd061d3e37fa957098d08f1c255e3076df312d31
Ashok Koda <kodaashok60@gmail.com>
2022-03-24 21:03:53 +0530

    Added:UC1 - Initializing variables

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -2,7 +2,14 @@ package com.bridgelabz.gambling;
 
 public class Gambler {
 
+	//Initialing variables
+	static int dailyStake = 100;
+	static int betInDollar = 1;
+
 	public static void main(String[] args) {
 		System.out.println("*********Welcome To Gambling Simulation************");
+
+		System.out.println("Total daily Stake is : " + dailyStake);
+		System.out.println("Betting Dollar for every game : " + betInDollar);
 	}
 }
