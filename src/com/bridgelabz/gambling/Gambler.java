a84a5d3ad3d745c89976002f83ba7f9f67737a86
Ashok Koda <kodaashok60@gmail.com>
2022-03-24 21:08:37 +0530

    Added:UC2 - Make  bet either win or loose

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -8,6 +8,15 @@ public class Gambler {
 
 	public static void main(String[] args) {
 		System.out.println("*********Welcome To Gambling Simulation************");
+		
+		int random = (int) ((Math.random() * 10) % 2);
+		if(random == 1) {
+			dailyStake++;
+			System.out.println("Won");
+		}else {
+			dailyStake--;
+			System.out.println("Loose");
+		}
 
 		System.out.println("Total daily Stake is : " + dailyStake);
 		System.out.println("Betting Dollar for every game : " + betInDollar);
