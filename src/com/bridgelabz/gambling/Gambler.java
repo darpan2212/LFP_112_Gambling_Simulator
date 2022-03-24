f24504464b2d6a58922c4ae7b72f26ec1d7681e9
Ashok Koda <kodaashok60@gmail.com>
2022-03-24 21:00:29 +0530

    Added:Welcome to Gambling Simulator

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -3,6 +3,6 @@ package com.bridgelabz.gambling;
 public class Gambler {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("*********Welcome To Gambling Simulation************");
 	}
 }
