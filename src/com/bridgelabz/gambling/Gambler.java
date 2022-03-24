a283eaa2e7b7343cffe2b3b030f1428e6d32d1df
smbiradar92 <smbiradar92@gmail.com>
2022-03-24 21:07:58 +0530

    Adding UC1, initial bet for every game

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.gambling;
 
+import java.util.Scanner;
+
 public class Gambler {
+		
+	public static void main(String[] args) {
 	
+		double stake = 100;
+		int N = 0;
+		
+		System.out.println("Welcome to Gambling simulator \n");
+		System.out.println("Enter number of games to bet your stake :");
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			stake++;;
+			System.out.println("You have bet a total stake value of $"+stake);
+		}
+	sc.close();
+	}
 	
 
 }
