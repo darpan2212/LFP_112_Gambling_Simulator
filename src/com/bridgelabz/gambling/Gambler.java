4cc77072495a1ab95cc5bc57335f5943e2fa08b3
smbiradar92 <smbiradar92@gmail.com>
2022-03-25 09:44:43 +0530

    UC3,Resign game after reaching the daily limit

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -1,39 +1,35 @@
 package com.bridgelabz.gambling;
 
-import java.util.Scanner;
-
 public class Gambler {
 
-	double stake = 100;
-	int N = 0;
-	double stakeValue;
+	double stakeValue = 100;
+	private int play;
 
 	public void gamePlay() {
 
-		System.out.println(
-				"Enter number of games to bet your stake :");
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+		double stakeWin = stakeValue * 1.5;
+		double stakeLose = stakeValue * 0.5;
+		
+		while (stakeValue < stakeWin
+				&& stakeValue > stakeLose) {
+			play = (int) Math.floor((Math.random()) * 2);
 
-		for (int i = 0; i < N; i++) {
-			double play = Math.random() * 2;
+			System.out.println(play);
 
 			if (play > 0.5) {
-				stake += 1;
+				stakeValue = stakeValue + 1;
 				System.out.println(
-						"Hurray!!! you won, your stake value is :"
-								+ stake);
+						"Hurray!!! you have won his play");
 			} else {
-				stake -= 1;
+				stakeValue = stakeValue -1;
 				System.out.println(
-						"Oops!!! you lose, your stake value is :"
-								+ stake);
+						"Oops!!! you lose this play. try Again..");
 			}
+			play++;
 			System.out.println(
 					"You have bet a total stake value of $"
-							+ stake);
+							+ stakeValue);
 		}
-		sc.close();
 	}
 
 	public static void main(String[] args) {
@@ -43,5 +39,6 @@ public class Gambler {
 
 		Gambler gambler = new Gambler();
 		gambler.gamePlay();
+		System.out.println("You have reached the maximum amount of your game,Please end your game");
 	}
 }
