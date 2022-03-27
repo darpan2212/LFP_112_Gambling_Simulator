bf69f45b32a21df882e4e37ba515c2c72f25f59d
smbiradar92 <smbiradar92@gmail.com>
2022-03-27 22:57:50 +0530

    UC4, Adding program to know total amount won or lost

--- src/com/bridgelabz/gambling/Gambler.java
+++ src/com/bridgelabz/gambling/Gambler.java
@@ -2,43 +2,80 @@ package com.bridgelabz.gambling;
 
 public class Gambler {
 
-	double stakeValue = 100;
+	static double stakeValue = 100;
 	private int play;
+	static int day = 0;
 
-	public void gamePlay() {
+	public void dailyPlay() {
+		double totalDailyAmount = 0;
+
+		Gambler gamble = new Gambler();
+		for (int day = 0; day <= 20; day++) {
+			totalDailyAmount = gamble.gamePlay(stakeValue,
+					day);
+			System.out.println(
+					"The total daily amount won for day "
+							+ day + " is $"
+							+ totalDailyAmount);
+		}
+		stakeValue = totalDailyAmount + 100;
+	}
+
+	public double gamePlay(double stakeValue, int day) {
 
 		double stakeWin = stakeValue * 1.5;
 		double stakeLose = stakeValue * 0.5;
-		
+
 		while (stakeValue < stakeWin
 				&& stakeValue > stakeLose) {
 			play = (int) Math.floor((Math.random()) * 2);
-
-			System.out.println(play);
-
+			double amount_win_lose = stakeValue;
 			if (play > 0.5) {
 				stakeValue = stakeValue + 1;
 				System.out.println(
-						"Hurray!!! you have won his play");
+						"Hurray!!! you have won this play,your current stakeValue is :$"
+								+ stakeValue);
 			} else {
-				stakeValue = stakeValue -1;
+				stakeValue = stakeValue - 1;
 				System.out.println(
-						"Oops!!! you lose this play. try Again..");
+						"Oops!!! you lose this play,your current stakeValue is :$"
+								+ stakeValue);
 			}
 			play++;
+			if (amount_win_lose > stakeValue) {
+				amount_win_lose = stakeValue
+						+ amount_win_lose;
+				System.out.println("The amount won for day "
+						+ day + " is : $"
+						+ amount_win_lose);
+			} else if (amount_win_lose < stakeValue) {
+				amount_win_lose = stakeValue
+						- amount_win_lose;
+				System.out
+						.println("The amount lost for day "
+								+ day + " is : $"
+								+ amount_win_lose);
+
+			}
 			System.out.println(
 					"You have bet a total stake value of $"
 							+ stakeValue);
+			System.out.println("===========================================");
+
 		}
+
+		System.out.println(
+				"You have reached the maximum amount of your game value,Please end your game");
+
+		return (stakeValue);
 	}
 
 	public static void main(String[] args) {
 
 		System.out.println(
 				"Welcome to Gambling simulator \n");
-
 		Gambler gambler = new Gambler();
-		gambler.gamePlay();
-		System.out.println("You have reached the maximum amount of your game,Please end your game");
+		gambler.dailyPlay();
 	}
+
 }
